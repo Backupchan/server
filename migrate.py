4f16ce3fb5b25849bf6d8361a0608741b7a6dfc6
IAmMoltony <koronavirusnyj@gmail.com>
2025-07-01 16:13:35 +0500

    Modify DB creation script to be for general migrations

---
+++ migrate.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+
+import database
+import serverconfig
+import logging
+import argparse
+import os
+
+def main():
+    logging.basicConfig(level=logging.INFO, format="[%(asctime)s] [%(name)s] [%(levelname)s]: %(message)s")
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("migration", help="Migration to apply. To run all migrations, don't pass this option.", type=str, default="")
+    args = parser.parse_args()
+
+    migration = args.migration.strip()
+
+    print("The Backup-chan database will now be migrated.")
+    server_config = serverconfig.get_server_config()
+    db = database.Database(server_config.get("db_path"), server_config.get("db"))
+    if migration == "":
+        db.initialize_database()
+    else:
+        with open(os.path.join("migrations", migration), "r", encoding="utf-8") as f:
+            sql = f.read()
+            db.run_migration(migration, sql)
+    print("The Backup-chan database has been migrated. Backup-chan is ready to run.")
+
+if __name__ == "__main__":
+    main()
