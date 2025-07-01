d6146f8e2705f67d5d5f55338c5d8932cb98f7d9
IAmMoltony <koronavirusnyj@gmail.com>
2025-07-01 19:25:31 +0500

    Remove leftover password config option

--- serverconfig.py
+++ serverconfig.py
@@ -7,7 +7,6 @@ import config
 def get_server_config():
     server_config = config.Config("./config.jsonc")
     server_config.add_option("db_path", str, "./backupchan.db")
-    server_config.add_option("webui_password", str, "123456")
     server_config.add_option("webui_debug", bool, False)
     server_config.add_option("temp_save_path", str, "/tmp/backupchan")
     server_config.add_option("db", dict, {})
