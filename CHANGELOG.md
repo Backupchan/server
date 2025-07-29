0c0447a10a9064a18d9a0368b25e5508bf54aee6
IAmMoltony <koronavirusnyj@gmail.com>
2025-07-29 13:18:49 +0500

    Fix stats not working

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 See what's changed between versions!
 
+## 1.1.1
+
+* Fixed `total_target_size` statistic being a string value.
+
 ## 1.1.0
 
 * Added `$M` syntax to name templates for whether the backup was manual or automatic.
--- stats.py
+++ stats.py
@@ -11,13 +11,13 @@ class Stats:
         total = 0
         for target in targets:
             total += self.db.get_target_size(target.id)
-        return total
+        return int(total)
 
     def total_recycle_bin_size(self) -> int:
         recycled_backups = self.db.list_recycled_backups()
         total = 0
         for backup in recycled_backups:
             total += self.db.get_backup(backup.id).filesize
-        return total
+        return int(total)
 
     # not worth putting total backups and targets count since they can be easily accessed from the database
