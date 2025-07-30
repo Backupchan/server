b82e000ab87d05869d87cba922f7aa1a36bb3437
IAmMoltony <koronavirusnyj@gmail.com>
2025-07-30 21:08:45 +0500

    Add script to serve using waitress

---
+++ waitress_serve.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Serve Backup-chan using waitress
+"""
+
+from waitress import serve
+from main import app
+
+# change these depending on your setup
+serve(app, host='0.0.0.0', port=5000, max_request_body_size=1024 ** 3 * 2)
