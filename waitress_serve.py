299481d746ef0d9e5fc609230e6ff9350d148a60
IAmMoltony <koronavirusnyj@gmail.com>
2025-07-30 23:41:06 +0500

    accidentally committed changes from my server

--- waitress_serve.py
+++ waitress_serve.py
@@ -7,4 +7,4 @@ from waitress import serve
 from main import app
 
 # change these depending on your setup
-serve(app, host='0.0.0.0', port=42069, max_request_body_size=1024 ** 3 * 10)
+serve(app, host='0.0.0.0', port=5000, max_request_body_size=1024 ** 3 * 2)
