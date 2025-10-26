5c75ea6c34021eb36bce0bb03d4e4fad8b85eab6
IAmMoltony <koronavirusnyj@gmail.com>
2025-10-26 17:05:00 +0500

    forgot to add the file

---
+++ scheduled_jobs/tmp_purge_job.py
@@ -0,0 +1,24 @@
+import scheduled_jobs
+import os
+import datetime
+
+class TemporaryPurgeJob(scheduled_jobs.ScheduledJob):
+    def __init__(self, interval: int, temp_dir: str):
+        super().__init__(interval, __name__.split(".")[-1])
+
+        self.temp_dir = temp_dir
+
+    def run(self):
+        temp_files = os.listdir(self.temp_dir)
+        for i, file in enumerate(temp_files):
+            temp_files[i] = os.path.join(self.temp_dir, file)
+        files = [file for file in temp_files if os.path.isfile(file)]
+        self.logger.info(files)
+        now = datetime.datetime.now()
+        for file in files:
+            mod_timestamp = os.path.getmtime(file)
+            mod_datetime = datetime.datetime.fromtimestamp(mod_timestamp)
+            file_age = now - mod_datetime
+            if file_age.days > 1:
+                self.logger.info("Deleting temporary file '%s' as it is older than 1 day", file)
+                os.remove(file)
