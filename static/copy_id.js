15d63c7e86ad965eef046e0f4170987697d035c7
IAmMoltony <koronavirusnyj@gmail.com>
2026-06-01 18:27:57 +0500

    WHY DOES IT HAVE TO ONLY WORK ON HTTPS AND NOT HTTP I HATE WEB DEV AIUWEFIWEJFWEOJ234J3JEOFIJMWEROFGIJWE OFIJWEOPIFJAWPERJIORPOGIJ

--- static/copy_id.js
+++ static/copy_id.js
@@ -1,4 +1,26 @@
+// navigator.clipboard.writeText doesn't work on non-secure connections
+function copyText(text) {
+	const textArea = document.createElement('textarea');
+	textArea.value = text;
+		  
+	textArea.style.position = 'fixed';
+	textArea.style.left = '-999999px';
+		  
+	document.body.appendChild(textArea);
+	textArea.focus();
+	textArea.select();
+
+	try {
+		document.execCommand('copy');
+	} catch (err) {
+		console.error(`failed to copy text: ${err}`);
+	}
+
+	document.body.removeChild(textArea);
+}
+
+
 document.querySelector("#copy-id-link").addEventListener("click", ev => {
-	navigator.clipboard.writeText(ev.target.dataset.targetId);
+	copyText(ev.target.dataset.targetId);
 	ev.target.innerText = "Copied!";
 });
